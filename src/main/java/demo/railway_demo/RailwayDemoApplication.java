3e4e2d36197d4f5f737c2122b98795338eb476dc
EduardoMR179 <eemr0001@ce.pucmm.edu.do>
2023-07-22 00:48:52 -0400

    First Commit

---
+++ src/main/java/demo/railway_demo/RailwayDemoApplication.java
@@ -0,0 +1,13 @@
+package demo.railway_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwayDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RailwayDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/demo/railway_demo/RailwayDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.railway_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailwayDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
